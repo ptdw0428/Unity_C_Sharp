263f06615cfee3c50f2e3544993625c5febc5356
Dongwoo_Seo <ptdw0428@naver.com>
2017-08-01 11:42:18 +0900

    Create FollowCam.cs
    LateUpdate 함수는 Update 함수 호출 이후 실행하기위해 사용.
    추적할 타깃의 이동이 종료 후 카메라가 추적
    카메라 위치 추적 Target의 dist 변수만큼 뒤쪽으로 배치, height만큼 위로 올림

---
+++ FollowCam.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+  
+  public Transform targetTr;  //Target Object Transform variable
+  public float dist = 10.0f;  //Distance to camera
+  public float height = 3.0f  //Camera height
+  public float dampTrace = 20.0f; //Smooth Follow Cam Variable;
+  
+  private Transform tr;   //Camera's Transform variable
+  
+  void Start(){
+    tr = GetComponent<Transform>();
+  }
+  
+  void LateUpdate(){
+    tr.position = Vector3.Lerp(tr.position, targetTr.position - (targetTr.forward * dist) + (Vector3.up * height), 
+    Time.deltaTime * dampTrace);
+    
+    tr.LookAt(targetTr, position);
+  }
+}
