6a2fe60966a5e4b7c27d03c40d0d611bc69d189d
albadw0428 <albadw0428@kau.kr>
2017-08-01 19:40:36 +0900

    수정

---
+++ WallCtrl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCtrl : MonoBehaviour
+{
+    void OnCollisionEnter(Collision coll)   //충돌이 시작할 때
+    {
+        if (coll.collider.tag == "BULLET")
+        {
+            Destroy(coll.gameObject);   //충돌한 GameObject 삭제
+        }
+    }
+}
